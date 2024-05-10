c1397c230337b505a4a023211f8bbc63ec8c6448
James Cowling <james@convex.dev>
2024-05-10 11:12:54 -0700

    comments on schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,15 +2,27 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+  // Cronvex table.
   crons: defineTable({
+    // Fully qualified function name.
     function: v.string(),
+
+    // Args as an object.
     args: v.any(),
-    // Only one of these two should be set.
-    ms: v.optional(v.float64()),
-    cronspec: v.optional(v.string()),
+
+    // // Optional unique name.
+    // name: v.optional(v.string()),
+
+    // Schedule, only one should be set.
+    ms: v.optional(v.float64()), // milliseconds
+    cronspec: v.optional(v.string()), // "* * * * *"
+
+    // References to scheduled jobs.
     schedulerJobId: v.optional(v.id("_scheduled_functions")),
     executionJobId: v.optional(v.id("_scheduled_functions")),
   }),
+
+  // Demo app table.
   syslog: defineTable({
     message: v.string(),
   }),
